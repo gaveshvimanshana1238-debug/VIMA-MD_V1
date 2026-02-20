f548cf6151c7827dfd4be7394a8a39db9cef3ab1
gaveshvimanshana1238-debug <gaveshvimanshana1238@gmail.com>
2026-02-20 14:05:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,17 +5,21 @@ if (fs.existsSync('config.env')) {
 }
 
 function convertToBool(text, fault = 'true') {
-  return text === fault ? true : false;
+  return text === fault;
 }
 
 module.exports = {
 
+  // Session ID
   SESSION_ID: process.env.SESSION_ID || "",
 
+  // Alive image
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/gaveshvimanshana126-cyber/VIMA-MD/main/Image/20260218_134730.jpg",
 
+  // Alive message (Changed to VIMA-MD)
   ALIVE_MSG: process.env.ALIVE_MSG || "*Hello 👋 VIMA-MD Is Alive Now 😍*",
 
-  BOT_OWNER: process.env.BOT_OWNER || '94742549935',
+  // Owner number
+  BOT_OWNER: process.env.BOT_OWNER || "94742549935",
 
 };
