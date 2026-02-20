58fe1a0c17d3397e8a89e8727cc758cf075e0e87
gaveshvimanshana1238-debug <gaveshvimanshana1238@gmail.com>
2026-02-20 14:01:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,21 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+if (fs.existsSync('config.env')) {
+  require('dotenv').config({ path: './config.env' });
+}
 
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+  return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
+  SESSION_ID: process.env.SESSION_ID || "",
 
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/gaveshvimanshana126-cyber/VIMA-MD/main/Image/20260218_134730.jpg",
+
+  ALIVE_MSG: process.env.ALIVE_MSG || "*Hello 👋 VIMA-MD Is Alive Now 😍*",
+
+  BOT_OWNER: process.env.BOT_OWNER || '94742549935',
 
 };
